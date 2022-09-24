117e956ac1972efb092de70785c21e6333250a14
Sai Varun <98223018+saivarun1410@users.noreply.github.com>
2022-09-24 23:17:21 +0530

    Time: 3 ms (50.47%), Space: 6 MB (46.50%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long nn=n;
-        double ans=1.0;
-        if (n<0) nn*=-1;
-        while (nn) {
-            if (nn%2==0) {
-                x*=x;
-                nn/=2;
+        double res=1.0;
+        long long power=n;
+        if(n<0) power*=-1;
+        while(power){
+            if(power%2==0){
+                 x*=x;
+                 power/=2;
             }
-            else {
-                ans*=x;
-                nn-=1;
+            else{
+                res=res*x;
+                power=power-1;
             }
         }
-        if (n<0) return 1/ans;
-        return ans;
+        if(n<0) res=1.0/res;
+        return res;
     }
 };
