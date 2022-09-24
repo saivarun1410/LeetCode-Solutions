ce9b8bb97f621a2e092b90b582fb8df24a12e814
Sai Varun <98223018+saivarun1410@users.noreply.github.com>
2022-09-25 00:17:05 +0530

    Time: 118 ms (16.07%), Space: 28.2 MB (87.06%) - LeetHub

--- 561-array-partition/561-array-partition.cpp
+++ 561-array-partition/561-array-partition.cpp
@@ -6,9 +6,7 @@ public:
         int sum=0;
         int count=0;
         for(int i=n-1;i>=0;i=i-2){
-            if(count==n/2) break;
             sum+=min(nums[i],nums[i-1]);
-            count++;
         }
         return sum;
     }
