b809e3edb570f0368036692d60b707d2558f7dbf
Sai Varun <98223018+saivarun1410@users.noreply.github.com>
2022-10-02 11:51:00 +0530

    Time: 5 ms (52.22%), Space: 6.2 MB (8.57%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,7 +3,8 @@ public:
     int f(int n, vector<int>&dp){
         if(n<=1) return n;
         if(dp[n]!=-1) return dp[n];
-        return dp[n]=f(n-1,dp)+f(n-2,dp);
+        dp[n]=f(n-1,dp)+f(n-2,dp);
+        return dp[n];
     }
     int fib(int n) {
         vector<int>dp(n+1,-1);
