a90891390307a9229c4d6302888ecb1039a65214
Sai Varun <98223018+saivarun1410@users.noreply.github.com>
2022-10-02 12:10:46 +0530

    Time: 3 ms (45.11%), Space: 6.5 MB (27.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
+    int unq(int m,int n,vector<vector<int>>&dp){
+        if(m==0||n==0) return 0;
+        if(m==1&&n==1) return 1;
+        if(dp[m][n]!=-1) return dp[m][n];
+        return dp[m][n]=unq(m-1,n,dp)+unq(m,n-1,dp);
+    }
     int uniquePaths(int m, int n) {
-        double res=1;
-        for (int i=1;i<=m-1;i++)
-            res=res*(n-1+i)/i;
-        return (int)res;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        int ans=unq(m,n,dp);
+        return ans;
     }
 };
