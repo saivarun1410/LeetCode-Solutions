718ce67834108674b5779337133d48c76ba6c721
Sai Varun <98223018+saivarun1410@users.noreply.github.com>
2022-10-03 11:30:48 +0530

    Time: 69 ms (12.91%), Space: 31.7 MB (13.20%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        for(int i=0;i<n;i++){
+            dp[i+1]=dp[i]+nums[i];
+        }
+        for(int i=0;i<n;i++){
+            if(dp[i]==dp[n]-dp[i+1]) return i;
+        }
+        return -1;
+    }
+};
