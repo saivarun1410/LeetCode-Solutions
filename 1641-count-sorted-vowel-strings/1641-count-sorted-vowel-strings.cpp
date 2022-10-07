b8fcebd7d4a4d42086de291684e6b4e3bed67af2
Sai Varun <98223018+saivarun1410@users.noreply.github.com>
2022-10-07 13:50:10 +0530

    Time: 4 ms (37.04%), Space: 6.3 MB (17.61%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int f(int ind, string s,int n){
+    int f(int ind, string s,int n,vector<vector<int>>&dp){
         if(n<=0) return 1;
         if(ind>=s.size()){
             return 0;
         }
-        int notPick=f(ind+1,s,n);
-        int Pick=f(ind,s,n-1);
-        return notPick+Pick;
+        if(dp[ind][n]!=-1) return dp[ind][n];
+        int notPick=f(ind+1,s,n,dp);
+        int Pick=f(ind,s,n-1,dp);
+        return dp[ind][n]=notPick+Pick;
     }
     int countVowelStrings(int n) {
         string s="aeiou";
-        if(n==1) return 5;
-        return f(0,s,n);
+        vector<vector<int>>dp(5,vector<int>(n+1,-1));
+        return f(0,s,n,dp);
     }
 };
