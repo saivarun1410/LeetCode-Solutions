5d2651dfd4f9a3cd498256569e5de7cfa449baa6
Sai Varun <98223018+saivarun1410@users.noreply.github.com>
2022-10-12 20:35:38 +0530

    Time: 347 ms (11.01%), Space: 79.2 MB (31.15%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -4,13 +4,8 @@ public:
         int n=pref.size();
         vector<int>ans;
         ans.push_back(pref[0]);
-        for(int i=1;i<n;i++){
-            if(pref[i]==0){
-                ans.push_back(pref[i-1]);
-            }
-            else{
-                ans.push_back(pref[i]^pref[i-1]);
-            }
+        for(int i=0;i<n-1;i++){
+            ans.push_back(pref[i]^pref[i+1]);
         }
         return ans;
     }
