63ca0d6c5c64f266c826943dac8f0630657e689a
Sai Varun <98223018+saivarun1410@users.noreply.github.com>
2022-10-12 20:53:13 +0530

    Time: 184 ms (11.47%), Space: 27.2 MB (29.37%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-    int f(int ind1,int ind2,string s1, string s2, vector<vector<int>>&dp){
-        int pick=0,notpick=0;
-        if(ind1<0||ind2<0) return 0;
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(s1[ind1]==s2[ind2]){
-            pick=1+f(ind1-1,ind2-1,s1,s2,dp);
-        }
-        else{
-            notpick=max(f(ind1,ind2-1,s1,s2,dp),f(ind1-1,ind2,s1,s2,dp));
-        }
-        return dp[ind1][ind2]=max(pick,notpick);
+    int f(int i, int j, string &s ,string &t, vector<vector<int>>&dp){
+    if(i<0||j<0) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    int res=0;
+    if(s[i]==t[j]){
+        res=1+f(i-1,j-1,s,t,dp);
     }
+    else{
+        res=max(f(i-1,j,s,t,dp),f(i,j-1,s,t,dp));
+       }
+    return dp[i][j]=res;
+}
+   
     int minInsertions(string s) {
-        string s1=s;
-        int l1=s.size();
-        int l2=s1.size();
-        reverse(s1.begin(),s1.end());
-        vector<vector<int>>dp(l1+1,vector<int>(l2+1,0));
-        for(int i=0;i<=l1;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<=l2;i++){
-            dp[0][i]=0;
-        }
-        for(int i=1;i<=l1;i++){
-            for(int j=1;j<=l2;j++){
-                if(s[i-1]==s1[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        int ans=dp[l1][l2];
-        return l1-ans;
+        string t=s;
+        int l=s.size();
+        reverse(t.begin(),t.end());
+        vector<vector<int>>dp(l+1,vector<int>(l+1,-1));
+        int ans=f(l-1,l-1,s,t,dp);
+        return l-ans;
     }
 };
