ec12f30550ebb323b520c13862b50ee1505fec16
Sai Varun <98223018+saivarun1410@users.noreply.github.com>
2022-10-16 12:21:56 +0530

    Time: 12 ms (44.87%), Space: 10.8 MB (20.71%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -2,24 +2,16 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         map<int,int>m;
-        map<int,int>n;
         int n1=nums1.size();
         int n2=nums2.size();
         for(int i=0;i<n1;i++){
             m[nums1[i]]++;
         }
-        for(int i=0;i<n2;i++){
-            n[nums2[i]]++;
-        }
         vector<int>v;
-        for(auto i=m.begin();i!=m.end();i++){
-            for(auto j=n.begin();j!=n.end();j++){
-                if(i->first==j->first){
-                    int mini=min(i->second,j->second);
-                    for(int k=1;k<=mini;k++){
-                        v.push_back(i->first);
-                    }
-                }
+        for(int i=0;i<n2;i++){
+            if(m[nums2[i]]){
+                v.push_back(nums2[i]);
+                m[nums2[i]]--;
             }
         }
         return v;
